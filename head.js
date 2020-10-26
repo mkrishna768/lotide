db84496bf164df37871cc3cb6e8116c3ddb30c99
Michael Krishna <michaelkrishna@hotmail.com>
2020-10-26 21:22:36 +0000

    Delinted head.js

--- head.js
+++ head.js
@@ -5,9 +5,10 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-const head = function(arr){
+const head = function(arr) {
   return arr[0];
-}
+};
+
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head(["a"]), "a");
