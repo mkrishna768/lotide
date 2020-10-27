feca51faa2f5880f7cf69d6e2436239221f5d56b
Michael Krishna <michaelkrishna@hotmail.com>
2020-10-27 01:37:31 +0000

    delinting

--- eqArrays.js
+++ eqArrays.js
@@ -5,24 +5,25 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-const eqArrays = function(a, b){
+const eqArrays = function(a, b) {
   if (a.length !== b.length) {
     return false;
   }
   for (let i = 0; i < a.length; i++) {
     if (a[i] !== b[i]) {
       return false;
-    };
+    }
     
   }
   return true;
-}
+};
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); 
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); 
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2"]), false);
 assertEqual(eqArrays([], []), true);
+
