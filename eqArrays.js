37bdb8f271b20257f632863e69f84329f83305d2
Michael Krishna <michaelkrishna@hotmail.com>
2020-10-29 23:53:30 +0000

    removed console.logs

--- eqArrays.js
+++ eqArrays.js
@@ -12,11 +12,9 @@ const eqArrays = function(a, b) {
   for (let i = 0; i < a.length; i++) {
     if (Array.isArray(a[i])) {
       if (!eqArrays(a[i], b[i])) {
-        console.log("eqarrays", a[i], b[i]);
         return false;
       }
     }else if (a[i] !== b[i]) {
-      console.log(`${a[i]} != ${b[i]}`);
       return false;
     }
     
