65f455a0863d83c48bbfee89d5a7e6546419b821
Michael Krishna <michaelkrishna@hotmail.com>
2020-11-02 21:03:24 +0000

    middle test using mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,24 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
+describe("#middle", () => {
+  it("returns empty for single item array", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns empty for 2 item array", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("returns 1 item for odd length array", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns 1 item for different odd length array", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("returns 2 items for even length array", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("returns 2 items for even length array", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
